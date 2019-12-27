46016468a1ac4487da57ba055ee5caf56386ed7a
Daria <daryalutkova@yandex.ru>
2019-12-27 11:26:30 +0300

    lab 10-11

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,87 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div
+      id="sensorWrap"
+      v-for="sensor in myCollectionOfBeautifulSensors"
+      :key="sensor.id"
+    >
+      <Sensor 
+      :id="sensor.id" :title="sensor.title" 
+      @removeSensor="remove"
+      />
+    </div>
+    <div class="input-group mb-3">
+      <h2>Add a New Sensor</h2>
+      <input type="text" class="form-control mt-3" placeholder="Enter Title" aria-label="Title" aria-describedby="basic-addon1" v-model="newBeautifulSensor" required>
+      <button type="button" class="btn btn-info mt-3" @click="addSensor">Add</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Sensor from "./components/Sensor.vue";
 
 export default {
   name: "app",
+  data: () => ({
+    myCollectionOfBeautifulSensors: [
+      {
+        id: 0,
+        title: "First beautiful sensor"
+      },
+      {
+        id: 1,
+        title: "Second beautiful sensor"
+      },
+      {
+        id: 2,
+        title: "Third beautiful sensor"
+      },
+      {
+        id: 3,
+        title: "Forth beautiful sensor"
+      },
+      {
+        id: 4,
+        title: "Fifth beautiful sensor"
+      },
+      {
+        id: 5,
+        title: "Sixth beautiful sensor"
+      },
+      {
+        id: 6,
+        title: "Seventh beautiful sensor"
+      },
+      {
+        id: 7,
+        title: "Eighth beautiful sensor"
+      },
+      {
+        id: 8,
+        title: "Nineth beautiful sensor"
+      },
+      {
+        id: 9,
+        title: "Tenth beautiful (ugly) sensor"
+      },
+    ],
+    newBeautifulSensor: null,
+    counter: 9,
+  }),
   components: {
-    HelloWorld
+    Sensor
+  },
+  methods: {
+    remove(e) {
+      this.myCollectionOfBeautifulSensors = this.myCollectionOfBeautifulSensors.filter((el) => {
+        return el.id != e
+      });
+    },
+    addSensor() {
+      this.counter++;
+      this.myCollectionOfBeautifulSensors.push({ id: this.counter, title: this.newBeautifulSensor })
+    }
   }
 };
 </script>
@@ -24,5 +94,20 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.input-group {
+  margin: 2rem;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: space-around;
+}
+
+.form-control {
+  width: 100% !important;
 }
 </style>
---
+++ src/components/Sensor.vue
@@ -0,0 +1,42 @@
+<template>
+  <div
+    :class="['card', { 'bg-success': state }, { 'bg-danger': !state }]"
+  >
+    <button @click="clickHandler" type="button" class="close" aria-label="Close">
+      <span aria-hidden="true">&times;</span>
+    </button>
+    <div class="card-body">
+      <h5 class="card-title">{{ id }}</h5>
+      <p class="card-text">{{ title }}</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ['id', 'title'],
+  computed: {
+    state() {
+      return Math.random() >= 0.5;
+    }
+  },
+  methods: {
+    clickHandler(){
+      this.$emit('removeSensor', this.id);
+    }
+  }
+};
+</script>
+<style>
+.card {
+  width: 20rem;
+  height: 10rem;
+  color: white;
+  margin-top: 1rem;
+}
+
+.close {
+  position: absolute;
+  right: 10px;
+  top: 5px;
+}
+</style>
